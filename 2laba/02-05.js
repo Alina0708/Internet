5bf9c1b969191047e178aa5fb0cd6d41d3b43077
kapkande <kapkande@gmail.com>
2023-02-23 15:15:43 +0300

    ready 2 labs

---
+++ 2laba/02-05.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require('fs').promises;
+const host = 'localhost';
+const port = 5000;
+
+const requestListener = function (req, res) {
+    fs.readFile(__dirname + "/fetch.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+
+        if (req.url === '/api/name') {
+            res.setHeader("Content-Type", "text/plain; charset=utf8");
+            res.writeHead(200);
+            return  res.end('Севрюк Алина Эдуардовна');
+          }
+  
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}/fetch`);
+});
